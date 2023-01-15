55a6baa571a3edac415ff587f5364b8c1d831942
Denis Krivosheev <denis.takeda@gmail.com>
2023-01-15 14:30:42 +0300

    Extend server configuration to store data in a file

--- cmd/server/internal/config/config.go
+++ cmd/server/internal/config/config.go
@@ -3,15 +3,22 @@ package config
 import (
 	"github.com/caarlos0/env/v6"
 	"github.com/pkg/errors"
+	"time"
 )
 
 type Config struct {
-	Address string `env:"ADDRESS"`
+	Address       string        `env:"ADDRESS"`
+	StoreInterval time.Duration `env:"STORE_INTERVAL"`
+	StoreFile     string        `env:"STORE_FILE"`
+	Restore       bool          `env:"RESTORE"`
 }
 
 func GetConfig() (Config, error) {
 	config := Config{
-		Address: "localhost:8080",
+		Address:       "localhost:8080",
+		StoreInterval: 300 * time.Second,
+		StoreFile:     "/tmp/devops-metrics-db.json",
+		Restore:       true,
 	}
 	if err := env.Parse(&config); err != nil {
 		return Config{}, errors.Wrap(err, "failed to parse server configuration from the environment variables")
