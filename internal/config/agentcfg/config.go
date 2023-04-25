ac8079f60d9e48383809da677323c9be24b5ae15
Denis Krivosheev <denis.takeda@gmail.com>
2023-04-25 10:32:37 +0300

    Support configuration of server and agent

--- internal/config/agentcfg/config.go
+++ internal/config/agentcfg/config.go
@@ -1,8 +1,11 @@
 package agentcfg
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"os"
 	"strings"
 	"time"
 
@@ -12,23 +15,47 @@ import (
 
 // Config is a configuration for agent
 type Config struct {
-	Address        string        `env:"ADDRESS"`
-	ReportInterval time.Duration `env:"REPORT_INTERVAL"`
-	PollInterval   time.Duration `env:"POLL_INTERVAL"`
-	Key            string        `env:"KEY"`
-	RateLimit      int           `env:"RATE_LIMIT"`
+	Config         string        `env:"CONFIG"`
+	Address        string        `env:"ADDRESS" json:"address"`
+	ReportInterval time.Duration `env:"REPORT_INTERVAL" json:"report_interval"`
+	PollInterval   time.Duration `env:"POLL_INTERVAL" json:"poll_interval"`
+	Key            string        `env:"KEY" json:"key"`
+	RateLimit      int           `env:"RATE_LIMIT" json:"rate_limit"`
 }
 
 // GetConfig extracts the configuration from environment variables and flags
 func GetConfig() (Config, error) {
-	config := Config{}
+	// Set default values
+	config := Config{
+		Address:        "http://localhost:8080",
+		ReportInterval: 10 * time.Second,
+		PollInterval:   2 * time.Second,
+		RateLimit:      1,
+	}
+
+	// Read from file
+	flag.StringVar(&config.Config, "c", "", "Path to configuration file")
+	flag.Parse()
+
+	if config.Config != "" {
+		content, err := os.ReadFile(config.Config)
+		if err != nil {
+			log.Fatal("Error when opening file: ", err)
+		}
+
+		// Now let's unmarshall the data into `payload`
+		err = json.Unmarshal(content, &config)
+		if err != nil {
+			log.Fatal("Error during Unmarshal(): ", err)
+		}
+	}
 
 	// Get flags
-	flag.StringVar(&config.Address, "a", "http://localhost:8080", "Server to send metrics to")
-	flag.DurationVar(&config.ReportInterval, "r", 10*time.Second, "Interval to send metrics to server")
-	flag.DurationVar(&config.PollInterval, "p", 2*time.Second, "Interval to collect metrics")
-	flag.StringVar(&config.Key, "k", "", "Key to sign")
-	flag.IntVar(&config.RateLimit, "l", 1, "The maximum amount of active requests")
+	flag.StringVar(&config.Address, "a", config.Address, "Server to send metrics to")
+	flag.DurationVar(&config.ReportInterval, "r", config.ReportInterval, "Interval to send metrics to server")
+	flag.DurationVar(&config.PollInterval, "p", config.PollInterval, "Interval to collect metrics")
+	flag.StringVar(&config.Key, "k", config.Key, "Key to sign")
+	flag.IntVar(&config.RateLimit, "l", config.RateLimit, "The maximum amount of active requests")
 	flag.Parse()
 
 	// Populate data from the env variables
